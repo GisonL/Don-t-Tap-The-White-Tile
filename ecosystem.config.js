d48b5a9abede5a357863812db47e9565db0f5c88
liuzk <liuzk@akulaku.com>
2019-03-08 10:26:09 +0800

    更改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       host: '111.230.110.59',
       ref: 'origin/master',
       repo: 'git@github.com:GisonL/Don-t-Tap-The-White-Tile.git',
-      path:'/ftp/node/production',
+      path: '/root/Don-t-Tap-The-White-Tile',
       'post-deploy':'git pull && npm -v'
       // 'post-deploy':'git pull && npm install && pm2 reload ecosystem.config.js --env production'
     }
