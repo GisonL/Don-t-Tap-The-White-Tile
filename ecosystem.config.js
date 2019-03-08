58ab68142e12fe393688bc885c48f391a3faa051
liuzk <liuzk@akulaku.com>
2019-03-08 10:11:11 +0800

    添加pm2配置文件

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  app: [
+    {
+      name: 'tap-tap_node',
+      script: './app.js',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+  deploy: {
+    production: {
+      user: 'root',
+      host: '111.230.110.59',
+      ref: 'origin/master',
+      repo: 'git@github.com:GisonL/Don-t-Tap-The-White-Tile.git',
+      path:'/ftp/node/production',
+      'post-deploy':'git pull && npm -v'
+      // 'post-deploy':'git pull && npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+}
